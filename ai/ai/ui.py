ace4940b4ca9c1db7f7835f7cfbbc0bf0a20924f
sakshamshahu <sakshamshahu.work@gmail.com>
2024-04-06 16:17:13 +0530

    feat: chat box
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- ai/ai/ui.py
+++ ai/ai/ui.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title('Read N Rag')
 st.write('Mine Text documents for information  💻 ⛏️ ')
+
+if prompt := st.chat_input('Whats Up ?'):
+    st.session_state['messages'].append({'role': 'user', 'content': prompt})
